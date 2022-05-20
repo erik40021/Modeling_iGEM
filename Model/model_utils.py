e6c409f1b80ad69697879c9a29ed0e931a7f1504
Erik Schüftan <erikschueftan@gmx.de>
2022-05-20 14:35:22 +0200

    added yeastGEM model and io functionality

---
+++ Model/model_utils.py
@@ -0,0 +1,30 @@
+
+
+def summarize_properties(model, short=False):
+    """ informs about model properties"""
+    print(f'\nModel summary:\n{len(model.reactions)} reactions')
+    print(f'{len(model.metabolites)} metabolites')
+    print(f'{len(model.genes)} genes')
+
+    if not short:
+        # Iterate through the objects in the model (details)
+        max_entries_per_object = 10
+        print(f"\nReactions (total: {len(model.reactions)})")
+        print("---------")
+        print("First %s reactions:" % max_entries_per_object)
+        for x in model.reactions[:max_entries_per_object]:
+            print("%s : %s" % (x.id, x.reaction))
+
+        print(f"\nMetabolites (total: {len(model.metabolites)})")
+        print("-----------")
+        print("First %s metabolites:" % max_entries_per_object)
+        for x in model.metabolites[:max_entries_per_object]:
+            print('%9s : %s' % (x.id, x.formula))
+
+        print(f"\nGenes (total: {len(model.genes)})")
+        print("-----")
+        print("First %s genes:" % max_entries_per_object)
+        for x in model.genes[:max_entries_per_object]:
+            associated_ids = (i.id for i in x.reactions)
+            print("%s is associated with reactions: %s" %
+                (x.id, "{" + ", ".join(associated_ids) + "}"))
