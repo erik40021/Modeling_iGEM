65d9f22fc8e141c25b17bc0033a155d8ac5d0fa2
Erik Schüftan <erikschueftan@gmx.de>
2022-05-24 19:30:25 +0200

    tsgukdrgud

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 from Data.yeastGEM_io import read_yeast_model
 from Model.model_utils import summarize_properties
+from cobra import Model, Reaction, Metabolite
 
 # step 1: load scaffold model
 model = read_yeast_model() # loads existing SBML yeastGEM model 
@@ -10,8 +11,9 @@ summarize_properties(model)
 
 # step 3: adjust model
 #TODO
-
-
+r1 = Reaction("Test_reaction1")
+r1.name = "test"
+summarize_properties(model)
 
 
 # last step: save model
