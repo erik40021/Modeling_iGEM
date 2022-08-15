b6a0b9ec3a71d00b4075ea27760fa6186c1ca97c
carasj <carasjaeckel@gmx.de>
2022-08-15 19:36:47 +0200

    reactions completed

--- yl_peroxisome.py
+++ yl_peroxisome.py
@@ -26,6 +26,8 @@ DMAPP = Metabolite(id="dmpp_x", formula="C5H9O7P2", name="dimethylallyl diphosph
 GPP = Metabolite(id="grdp_x", formula="C10H17O7P2", name="geranyl diphosphate[p]", charge=-3, compartment="x")
 Alpha_pinene = Metabolite(id="0000_x", formula="C6H16", name="(+)-alpha-pinene[p]", charge=0, compartment="x")#metabolite not excisting in any other compartment
 Phosphate = model.metabolites.get_by_id("pi_x")
+FPP = Metabolite(id="s_4280", formula="C15H25O7P2", name="farnesyl diphosphate[p]", charge=-3, compartment="p")
+NPP = Metabolite(id="s_4281",formula='C10H17O7P2', name='neryl diphosphate', charge=-3, compartment='p')
 CO2 = model.metabolites.get_by_id("co2_x")
 Diphosphate = model.metabolites.get_by_id("ppi_x")
 
@@ -39,8 +41,13 @@ Erg8_reaction = Reaction(id="MVA5", name="1.0 Mevalonate-P + 1.0 ATP --> 1.0 Mev
 Erg19_reaction = Reaction(id="MVA6", name="1.0 Mevalonate-PP + 1.0 ATP --> 1.0 IPP + 1.0 ADP + 1.0 PO4 + 1.0 CO2", lower_bound=None, upper_bound=1000.0) #irreversible reactions, so lower_bound = None
 Idi1_reaction = Reaction(id="MVA7", name="1.0 IPP <--> 1.0 DMAPP", upper_bound=1000.0)
 
-mFPS144_reaction = Reaction(id="MVA8", name="1.0 IPP + 1.0 DMAPP --> 1.0 GPP + 1.0 Diphosphate", upper_bound=1000.0)
-APS_reaction = Reaction(id="MVA9", name="1.0 GPP --> 1.0 Alpha-Pinene + 1.0 Diphosphate", upper_bound=1000.0)
+AgGPPS2_reaction = Reaction(id="MVA8", name="1.0 IPP + 1.0 DMAPP --> 1.0 GPP + 1.0 Diphosphate", upper_bound=1000.0)
+mFPS144_GPP_reaction = Reaction(id="MVA9", name="1.0 IPP + 1.0 DMAPP --> 1.0 GPP + 1.0 Diphosphate", upper_bound=1000.0)
+mFPS144_FPP_reaction = Reaction(id="MVA10", name="1.0 IPP + 1.0 DMAPP --> 1.0 FPP + 1.0 Diphosphate", upper_bound=1000.0)
+SiNPPS1_reaction = Reaction(id="MVA11", name="1.0 IPP + 1.0 DMAPP --> 1.0 NPP + 1.0 Diphosphate", upper_bound=1000.0)
+APS_npp_reaction = Reaction(id="MVA12", name="1.0 NPP --> 1.0 Alpha-Pinene + 1.0 Diphosphate", upper_bound=1000.0)
+APS_gpp_reaction = Reaction(id="MVA13", name="1.0 GPP --> 1.0 Alpha-Pinene + 1.0 Diphosphate", upper_bound=1000.0)
+APinene_con_reaction = Reaction(id="r_apinene_con", name="1.0 Alpha-Pinene ->", upper_bound=1000.0)
 
 # Add metabolites and reaction stoichiometry to reaction objects
 Erg10_reaction.add_metabolites({
@@ -94,17 +101,41 @@ Idi1_reaction.add_metabolites({
     DMAPP: 1.0
 }, reversibly=True)
 
-mFPS144_reaction.add_metabolites({
+mFPS144_GPP_reaction.add_metabolites({
     IPP: -1.0,
     DMAPP: -1.0,
     GPP: 1.0,
     Diphosphate: 1.0
 })
 
-APS_reaction.add_metabolites({
+mFPS144_FPP_reaction.add_metabolites({
+    GPP: -1.0,
+    IPP: -1.0,
+    FPP: 1.0,
+    Diphosphate: 1.0
+})
+
+APS_gpp_reaction.add_metabolites({
     GPP: -1.0,
     Alpha_pinene: 1.0,
     Diphosphate: 1.0
+})
+
+APS_npp_reaction.add_metabolites({
+    NPP: -1.0,
+    Alpha_pinene: 1.0,
+    Diphosphate: 1.0
+})
+
+SiNPPS1_reaction.add_metabolites({
+    IPP: -1.0,
+    DMAPP: -1.0,
+    NPP: 1.0,
+    Diphosphate: 1.0
+})
+
+APinene_con_reaction.add_metabolites({
+    Alpha_pinene: -1.0
 })
 
 # Add reactions to model
@@ -116,8 +147,13 @@ reactionlist = [
     Erg8_reaction,
     Erg19_reaction,
     Idi1_reaction,
-    mFPS144_reaction,
-    APS_reaction
+    mFPS144_GPP_reaction, #7
+    mFPS144_FPP_reaction,
+    AgGPPS2_reaction,
+    SiNPPS1_reaction,
+    APS_gpp_reaction,
+    APS_npp_reaction,
+    APinene_con_reaction
 ]
 
 for reaction in reactionlist:
