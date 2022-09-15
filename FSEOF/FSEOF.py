e46ddc3eb26b797593a17a80cd77776ff758fe20
LasseMiddendorf <lasse.m@gmx.de>
2022-09-15 11:27:05 +0200

    Update FSEOF.py

--- FSEOF/FSEOF.py
+++ FSEOF/FSEOF.py
@@ -49,7 +49,7 @@ class FSEOF():
         self.model.objective = self.model.reactions.get_by_id(self.biomassID)
         self.maxProductFlux = self.solution.objective_value
     
-    def find_targets(self, steps, maxFluxCutoff=0.95):
+    def find_targets(self, steps: int, maxFluxCutoff=0.95, useFVA=True, constrainBiomass=False):
         
         """
         DESCRIPTION:
@@ -64,100 +64,127 @@ class FSEOF():
         fluxCapacity = pd.DataFrame()
         enforcedFluxes = list()
 
-        #constrain growth rate to 95% of optimal growth rate
-        #growthConstraint = self.model.problem.Constraint(
-        #    self.model.reactions.get_by_id(self.biomassID).flux_expression, lb = maxFluxCutoff * self.optimalGrowth
-        #)
-        #self.model.add_cons_vars(growthConstraint)
-        
-
-        for i in range(steps - 1):
-            
-            #calulate current enforced flux and add it as constraint to the model    
-            lb = self.initialProductFlux + (i/steps) * (self.maxProductFlux - self.initialProductFlux)
-            constraint = self.model.problem.Constraint(
-                self.model.reactions.get_by_id(self.objectiveID).flux_expression, lb=lb
-            )
-            self.model.add_cons_vars(constraint)
-
-            #store enforced flux in enforcedFluxes list for x-values of linear regression
-            enforcedFluxes.append(lb)
-
-            #calculate FVA, Vavg, and flux capacity for all reaction with the current enforced flux
-            FVA = flux_variability_analysis(self.model)
-            fluxes["Average_{}".format(i)] = (FVA["maximum"] + FVA["minimum"]) / 2
-            fluxCapacity["Flux_capacity_{}".format(i)] = FVA["maximum"] - FVA["minimum"]
-
         #define helper functions for linear regression
         def f(x, m, b):
             return m*x+b
-        
+            
         def getSlopes(y):
             popt, _ = curve_fit(f, enforcedFluxes, y)
             return popt[0]
+
+        if constrainBiomass == True:
+            #constrain growth rate to 95% of optimal growth rate
+            growthConstraint = self.model.problem.Constraint(
+                self.model.reactions.get_by_id(self.biomassID).flux_expression, lb = maxFluxCutoff * self.optimalGrowth
+            )
+            self.model.add_cons_vars(growthConstraint)
         
-        #calculate slopes of linear regressions for fluxes and flux capactiy
-        fluxes["q_slope"] = fluxes.apply(lambda y: getSlopes(y), axis=1)
-        fluxCapacity["l_sol"] = fluxCapacity.apply(lambda y: getSlopes(y), axis=1)
+        if useFVA == True:
+
+            for i in range(steps - 1):
+                print("\nPERFORMING FVA No. {}\n".format(i+1))
+                #calulate current enforced flux and add it as constraint to the model    
+                lb = self.initialProductFlux + (i/steps) * (self.maxProductFlux - self.initialProductFlux)
+                constraint = self.model.problem.Constraint(
+                    self.model.reactions.get_by_id(self.objectiveID).flux_expression, lb=lb
+                )
+                self.model.add_cons_vars(constraint)
+
+                #store enforced flux in enforcedFluxes list for x-values of linear regression
+                enforcedFluxes.append(lb)
+
+                #calculate FVA, Vavg, and flux capacity for all reaction with the current enforced flux
+                FVA = flux_variability_analysis(self.model)
+                fluxes["Average_{}".format(i)] = (FVA["maximum"] + FVA["minimum"]) / 2
+                fluxCapacity["Flux_capacity_{}".format(i)] = FVA["maximum"] - FVA["minimum"]
+
+
+            
+            #calculate slopes of linear regressions for fluxes and flux capactiy
+            fluxes["q_slope"] = fluxes.apply(lambda y: getSlopes(y), axis=1)
+            fluxCapacity["l_sol"] = fluxCapacity.apply(lambda y: getSlopes(y), axis=1)
+            
+            #Store list of targets
+            self.targets = pd.concat([fluxes["q_slope"], fluxCapacity["l_sol"]], axis=1)
         
-        #Store list of targets
-        self.targets = pd.concat([fluxes["q_slope"], fluxCapacity["l_sol"]], axis=1)
+        else:
+            
+            for i in range(steps - 1):
+                
+                lb = self.initialProductFlux + (i/steps) * (self.maxProductFlux - self.initialProductFlux)
+                constraint = self.model.problem.Constraint(
+                    self.model.reactions.get_by_id(self.objectiveID).flux_expression, lb=lb
+                )
+                self.model.add_cons_vars(constraint)
+
+                enforcedFluxes.append(lb)
+
+                solution = self.model.optimize()
+                fluxes["Average_{}".format(i)] = solution.fluxes
+            
+            fluxes["q_slope"] = fluxes.apply(lambda y: getSlopes(y), axis=1)               
+            self.targets = fluxes
  
-    def sort_targets(self):
+    def sort_targets(self, useFVA=False):
         
-        #The helper function classify() is used classify the type of the correlation of q_slope and l_sol
-        #A value of 1 is assigend for a pos. correlation, -1 is assigend for neg. correlations and 0 if there is
-        #no clear correlation between the tested reaction and reaction of interest
+        if useFVA == True:
+
+            #The helper function classify() is used classify the type of the correlation of q_slope and l_sol
+            #A value of 1 is assigend for a pos. correlation, -1 is assigend for neg. correlations and 0 if there is
+            #no clear correlation between the tested reaction and reaction of interest
         
-        def classify(x):
-            if x > 1:
-                return 1
-            elif x < 1:
-                return -1
-            else:
-                return 0
-
-        #Classify the calculated q_slopes and l_sol
-        self.targets["q_slope_classifier"] = self.targets["q_slope"].apply(classify)
-        self.targets["l_sol_classifier"] = self.targets["l_sol"].apply(classify)
-
-        #Assign class of tested reaction
-        def helper(q_slope_classifier, l_sol_classifier):
-            if q_slope_classifier == 1:
-                if l_sol_classifier == 1:
+            def classify(x):
+                if x > 1:
                     return 1
-                elif l_sol_classifier == -1:
-                    return 2
+                elif x < 1:
+                    return -1
                 else:
-                    return 3
-            elif q_slope_classifier == -1:
-                if l_sol_classifier == 1:
-                    return 4
-                elif l_sol_classifier == -1:
-                    return 5
+                    return 0
+
+            #Classify the calculated q_slopes and l_sol
+            self.targets["q_slope_classifier"] = self.targets["q_slope"].apply(classify)
+            self.targets["l_sol_classifier"] = self.targets["l_sol"].apply(classify)
+
+            #Assign class of tested reaction
+            def helper(q_slope_classifier, l_sol_classifier):
+                if q_slope_classifier == 1:
+                    if l_sol_classifier == 1:
+                        return 1
+                    elif l_sol_classifier == -1:
+                        return 2
+                    else:
+                        return 3
+                elif q_slope_classifier == -1:
+                    if l_sol_classifier == 1:
+                        return 4
+                    elif l_sol_classifier == -1:
+                        return 5
+                    else:
+                        return 6
                 else:
-                    return 6
-            else:
-                if l_sol_classifier == 1:
-                    return 7
-                elif l_sol_classifier == -1:
-                    return 8
-                else:
-                    return 9
+                    if l_sol_classifier == 1:
+                        return 7
+                    elif l_sol_classifier == -1:
+                        return 8
+                    else:
+                        return 9
 
 
-        self.targets["reaction_class"] = self.targets[["q_slope_classifier", "l_sol_classifier"]].apply(
-            lambda x: helper(*x), axis=1
-        )
+            self.targets["reaction_class"] = self.targets[["q_slope_classifier", "l_sol_classifier"]].apply(
+                lambda x: helper(*x), axis=1
+            )
 
-        #Sort list of reations by their reaction class
-        self.targets.sort_values("reaction_class", inplace=True)
+            #Sort list of reations by their reaction class
+            self.targets.sort_values("reaction_class", inplace=True)
+
+        else:
+            self.targets.sort_values("q_slope", ascending=False, inplace=True)
 
 
-    def run(self):
+    def run(self, steps):
         self.calculate_intitial_fluxes()
         self.calculate_maxmimal_product_flux()
-        self.find_targets() #add option to change maxCutoff?
+        self.find_targets(steps) #add option to change maxCutoff?
 
 # TO-DO:
 # -why step -1 ?
